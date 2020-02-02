8ad85bd51ebd900db90e220cf96166ba55949834
Caroline Vetter <carolinesvetter@gmail.com>
2020-02-02 14:47:47 +0100

    changed package name, optimized Fetcher, rebuild WebsiteWriter, added class Website

---
+++ src/csv/websitefetcher/Main.java
@@ -0,0 +1,30 @@
+package csv.websitefetcher;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Fetcher fetcher = new Fetcher();
+        WebsiteWriter wb = new WebsiteWriter();
+        String url = "https://t3n.de/news/";
+        Website page = new Website(url, getDate(), fetcher.getWebsiteTitle(url));
+        page.setHtml(fetcher.fetchWebsite(page.getUrl()));
+        page.setLinks(fetcher.getLinksFromWebsite(page.getUrl()));
+        output(page.getLinks().toString());
+        wb.saveWebsite(page);
+    }
+
+    private static void output(String s) {
+        System.out.println(s);
+    }
+
+    public static String getDate() {
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+        return dateFormat.format(date);
+    }
+}
---
+++ src/csv/websitefetcher/Website.java
@@ -0,0 +1,70 @@
+package csv.websitefetcher;
+
+import java.util.List;
+
+public class Website {
+
+    private String title;
+    private String url;
+    private List<String> links;
+    private String fetchedOnDate;
+    private StringBuilder html;
+
+    Website(String url, String fetchedOnDate, String title) {
+        this.url = url;
+        this.fetchedOnDate = fetchedOnDate;
+        this.title = title;
+    }
+
+    Website(String url, String fetchedOnDate) {
+        this.url = url;
+        this.fetchedOnDate = fetchedOnDate;
+    }
+
+    Website(String url) {
+        this.url = url;
+    }
+
+    Website() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public List<String> getLinks() {
+        return links;
+    }
+
+    public void setLinks(List<String> links) {
+        this.links = links;
+    }
+
+    public String getFetchedOnDate() {
+        return fetchedOnDate;
+    }
+
+    public void setFetchedOnDate(String fetchedOnDate) {
+        this.fetchedOnDate = fetchedOnDate;
+    }
+
+    public StringBuilder getHtml() {
+        return html;
+    }
+
+    public void setHtml(StringBuilder html) {
+        this.html = html;
+    }
+}
